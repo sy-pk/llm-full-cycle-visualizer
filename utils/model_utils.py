2d3e027d8bae25bb071b40d8523ff1ef2ac0d5ec
sy-pk <173501972+sy-pk@users.noreply.github.com>
2025-12-11 17:14:14 -0600

    fix: remove model.to cpu

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -5,7 +5,6 @@ def load_model(model_name="gpt2"):
     model = AutoModelForCausalLM.from_pretrained(
         model_name, device_map=None, low_cpu_mem_usage=False
     )
-    model.to("cpu")
     model.eval()
     return model
 
